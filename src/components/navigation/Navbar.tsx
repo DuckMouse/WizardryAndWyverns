784fb07d52f03750240573eb2943b0fc9c3223d7
Dewald Bez <dewald15@gmail.com>
2024-03-09 15:06:03 +1300

    Moved the sign-in button to the right-hand side with a profile placeholder

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Link } from "react-router-dom";
 import { UserAuth } from "../../context/AuthContext";
@@ -8,6 +8,10 @@ import teampicklogo from "../..//assets/images/teampick_logo.png";
 export const TopNavBar = () => {
   const { user, logOut } = UserAuth();
   const handleSignOut = logOut;
+
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(" ");
+  }
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
@@ -44,18 +48,67 @@ export const TopNavBar = () => {
                         );
                       } else return null;
                     })}
-                    {user && !user?.isAnonymous ? (
-                      <li className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-                        <Link to={"#"} onClick={handleSignOut}>
-                          Log out
-                        </Link>
-                      </li>
-                    ) : (
-                      <li className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-                        <Link to={"signin"}>Sign In</Link>
-                      </li>
-                    )}
                   </div>
+                </div>
+              </div>
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex items-center">
+                  {/* Profile dropdown */}
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=1935&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        {user && !user?.isAnonymous ? (
+                          <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                              onClick={handleSignOut}
+                            >
+                              Sign out
+                            </a>
+                          )}
+                        </Menu.Item>
+                        ) : (
+                          <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="/signin"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Sign In
+                            </a>
+                          )}
+                        </Menu.Item>
+                        )}                        
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
                 </div>
               </div>
             </div>
@@ -64,5 +117,4 @@ export const TopNavBar = () => {
       )}
     </Disclosure>
   );
-
-}
+};
