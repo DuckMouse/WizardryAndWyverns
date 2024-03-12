ca2de0749db8309a0aa519ca1ac832a31ba2eedb
DuckMouse <auretswanepoel@gmail.com>
2024-03-12 05:00:45 +0000

    fix linting for protected component

--- src/routes/Protected.tsx
+++ src/routes/Protected.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import type React from "react";
 import { Navigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
-export const Protected = ({ children }: any) => {
+export interface ProtectedProps {
+	children: React.JSX.Element;
+}
+
+export const Protected = ({ children }: ProtectedProps) => {
 	const { user } = UserAuth();
 	if (!user) {
 		return <Navigate to="/" replace />;
