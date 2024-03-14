43a52e0de5fb9c95c18cf61e6a7c9e6058720677
Kean Bayneslow <kean.bayneslow@gmail.com>
2024-03-14 12:58:08 +1100

    updates production app in config file and completed functional  tests

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyBB720h_2ddm7EGXx3PoxqyWkL6MMCceuo",
-  authDomain: "teampick-1d391.firebaseapp.com",
-  projectId: "teampick-1d391",
-  storageBucket: "teampick-1d391.appspot.com",
-  messagingSenderId: "627066458377",
-  appId: "1:627066458377:web:bb54258140a8cabd3843d8",
-  measurementId: "G-J142L3J4NK"
+	authDomain: "teampick-1d391.firebaseapp.com",
+	projectId: "teampick-1d391",
+	storageBucket: "teampick-1d391.appspot.com",
+	messagingSenderId: "627066458377",
+	appId: "1:627066458377:web:bb54258140a8cabd3843d8",
+	measurementId: "G-J142L3J4NK"
 };
 
 const app = initializeApp(firebaseConfig);
